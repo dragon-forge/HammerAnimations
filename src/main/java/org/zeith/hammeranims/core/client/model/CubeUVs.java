0d13dd4871802eea87ab021edb2737e5106071d7
Zeith <me@zeith.org>
2024-04-08 00:42:45 +0300

    [1.16.5] Release 16.5.25.

--- src/main/java/org/zeith/hammeranims/core/client/model/CubeUVs.java
+++ src/main/java/org/zeith/hammeranims/core/client/model/CubeUVs.java
@@ -28,30 +28,37 @@ public interface CubeUVs
 		@Override
 		public SizedUV get(EnumFacing direction)
 		{
-			return switch(direction)
+			switch(direction)
 			{
-				case EAST -> SizedUV.fromFloats(uv.x(),
-						uv.y() + depth(), uv.x() + depth(), uv.y() + depth() + height()
-				);
-				case WEST -> SizedUV.fromFloats(
-						uv.x() + depth() + width(),
-						uv.y() + depth(), uv.x() + depth() + width() + depth(), uv.y() + depth() + height()
-				);
-				case DOWN -> SizedUV.fromFloats(
-						uv.x() + depth() + width(), uv.y() + depth(), uv.x() + depth() + width() + width(), uv.y());
-				case UP -> SizedUV.fromFloats(
-						uv.x() + depth(), uv.y(), uv.x() + depth() + width(), uv.y() + depth());
-				case NORTH -> SizedUV.fromFloats(
-						uv.x() + depth(),
-						uv.y() + depth(), uv.x() + depth() + width(), uv.y() + depth() + height()
-				);
-				case SOUTH -> SizedUV.fromFloats(
-						uv.x() + depth() + width() + depth(),
-						uv.y() + depth(),
-						uv.x() + depth() + width() + depth() + width(), uv.y() + depth() + height()
-				);
-				default -> throw new IllegalStateException("Wtf?");
-			};
+				case EAST:
+					return SizedUV.fromFloats(uv.x(),
+							uv.y() + depth(), uv.x() + depth(), uv.y() + depth() + height()
+					);
+				case WEST:
+					return SizedUV.fromFloats(
+							uv.x() + depth() + width(),
+							uv.y() + depth(), uv.x() + depth() + width() + depth(), uv.y() + depth() + height()
+					);
+				case DOWN:
+					return SizedUV.fromFloats(
+							uv.x() + depth() + width(), uv.y() + depth(), uv.x() + depth() + width() + width(), uv.y());
+				case UP:
+					return SizedUV.fromFloats(
+							uv.x() + depth(), uv.y(), uv.x() + depth() + width(), uv.y() + depth());
+				case NORTH:
+					return SizedUV.fromFloats(
+							uv.x() + depth(),
+							uv.y() + depth(), uv.x() + depth() + width(), uv.y() + depth() + height()
+					);
+				case SOUTH:
+					return SizedUV.fromFloats(
+							uv.x() + depth() + width() + depth(),
+							uv.y() + depth(),
+							uv.x() + depth() + width() + depth() + width(), uv.y() + depth() + height()
+					);
+				default:
+					throw new IllegalStateException("Wtf?");
+			}
 		}
 		
 		public int width()
