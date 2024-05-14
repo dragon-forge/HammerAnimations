882f54a27a453fb212e9f7385f024437bf441a3f
Zeith <me@zeith.org>
2024-05-14 11:03:10 +0300

    [1.19.2] Cleanup player map when shutting down server.

--- src/main/java/org/zeith/hammeranims/core/contents/commands/CommandBedrock.java
+++ src/main/java/org/zeith/hammeranims/core/contents/commands/CommandBedrock.java
@@ -4,6 +4,7 @@ import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.event.server.ServerStoppingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -14,6 +15,12 @@ public class CommandBedrock
 	public static void registerCmd(RegisterCommandsEvent e)
 	{
 		e.getDispatcher().register(register());
+	}
+	
+	@SubscribeEvent
+	public static void serverShutdown(ServerStoppingEvent e)
+	{
+		CommandParticle.PLAYER_CUSTOM_MAP.clear();
 	}
 	
 	public static LiteralArgumentBuilder<CommandSourceStack> register()
