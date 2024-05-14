5f81e3a7b5bf8a869ff0e65851d0607f7ebecb77
Zeith <me@zeith.org>
2024-05-14 22:57:42 +0300

    [1.20.1] 20.1.32, fixed server-side crash and reduce total particle render type count.

---
+++ src/main/java/org/zeith/hammeranims/core/client/particle/ParticleMaterialRenderType.java
@@ -0,0 +1,45 @@
+package org.zeith.hammeranims.core.client.particle;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.BufferBuilder;
+import com.mojang.blaze3d.vertex.Tesselator;
+import net.minecraft.client.particle.ParticleRenderType;
+import net.minecraft.client.renderer.texture.TextureManager;
+import org.zeith.hammeranims.api.particles.ParticleMaterial;
+
+public class ParticleMaterialRenderType
+		implements ParticleRenderType
+{
+	public static final ParticleMaterialRenderType[] BUILTIN = {
+			new ParticleMaterialRenderType(ParticleMaterial.OPAQUE),
+			new ParticleMaterialRenderType(ParticleMaterial.ALPHA),
+			new ParticleMaterialRenderType(ParticleMaterial.BLEND),
+			new ParticleMaterialRenderType(ParticleMaterial.ADDITIVE)
+	};
+	
+	protected final ParticleMaterial material;
+	
+	public ParticleMaterialRenderType(ParticleMaterial material)
+	{
+		this.material = material;
+	}
+	
+	@Override
+	public void begin(BufferBuilder pBuilder, TextureManager pTextureManager)
+	{
+		material.beginGL();
+		RenderSystem.depthMask(true);
+	}
+	
+	@Override
+	public void end(Tesselator pTesselator)
+	{
+		material.endGL();
+	}
+	
+	@Override
+	public String toString()
+	{
+		return "HammerAnimations_ParticleMaterialRenderType_" + material.name();
+	}
+}
