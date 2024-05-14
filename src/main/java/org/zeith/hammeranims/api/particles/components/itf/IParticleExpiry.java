f3f92631cf0c3e350f471e85ac91b1e16301817f
Zeith <me@zeith.org>
2024-05-14 08:16:18 +0300

    [1.16.5] IParticleExpiry

---
+++ src/main/java/org/zeith/hammeranims/api/particles/components/itf/IParticleExpiry.java
@@ -0,0 +1,11 @@
+package org.zeith.hammeranims.api.particles.components.itf;
+
+import org.zeith.hammeranims.api.particles.components.IParticleComponent;
+import org.zeith.hammeranims.api.particles.emitter.BedrockParticle;
+import org.zeith.hammeranims.api.particles.emitter.ParticleEmitter;
+
+public interface IParticleExpiry
+		extends IParticleComponent
+{
+	void expire(ParticleEmitter emitter, BedrockParticle particle);
+}
