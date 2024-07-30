cc0c060e31b91a1570a4d64589da2a7eeae9011c
Zeith <me@zeith.org>
2024-07-31 01:02:56 +0300

    [1.12.2] More lenient handling of geometry version;

--- src/main/java/org/zeith/hammeranims/core/impl/api/geometry/decoder/GsonGeometryDecoder.java
+++ src/main/java/org/zeith/hammeranims/core/impl/api/geometry/decoder/GsonGeometryDecoder.java
@@ -6,6 +6,7 @@ import com.zeitheron.hammercore.utils.java.tuples.Tuple2;
 import com.zeitheron.hammercore.utils.java.tuples.Tuples;
 import lombok.val;
 import net.minecraft.util.ResourceLocation;
+import org.zeith.hammeranims.HammerAnimations;
 import org.zeith.hammeranims.api.geometry.IGeometryContainer;
 import org.zeith.hammeranims.api.utils.EmbeddedLocation;
 import org.zeith.hammeranims.core.client.render.vertex.VertexType;
@@ -65,7 +66,7 @@ public class GsonGeometryDecoder
 			if(entry.getKey().equals("format_version"))
 			{
 				String formatVersion = GsonHelper.convertToString(entry.getValue(), entry.getKey());
-				checkFormatVersion(formatVersion);
+				checkFormatVersion(fileLocation, formatVersion);
 			} else if(entry.getKey().equals("minecraft:geometry"))
 			{
 				Tuple2<EmbeddedLocation, GeometryDataImpl> identifierAndModel = parseGeometry(container, fileLocation, GsonHelper.convertToJsonArray(entry.getValue(), entry.getKey()));
@@ -196,12 +197,12 @@ public class GsonGeometryDecoder
 		return part;
 	}
 	
-	private static void checkFormatVersion(String version)
+	private static void checkFormatVersion(ResourceLocation fileLocation, String version)
 	{
-		if(!contains(ACCEPTABLE_FORMAT_VERSIONS, version)) throw new JsonSyntaxException("The format version "
-																						 + version + " is not supported. Supported versions: "
-																						 + Arrays.toString(ACCEPTABLE_FORMAT_VERSIONS)
-		);
+		if(!contains(ACCEPTABLE_FORMAT_VERSIONS, version))
+			HammerAnimations.LOG.warn("[{}]: Potentially unsupported version of geometry {}. Supported versions: {}",
+					fileLocation, version, Arrays.toString(ACCEPTABLE_FORMAT_VERSIONS)
+			);
 	}
 	
 	public static <T> boolean contains(T[] array, T object)
