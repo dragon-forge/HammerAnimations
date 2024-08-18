70c337852ea35c9334f7802b26fca3a36e180955
Zeith <me@zeith.org>
2024-08-18 20:30:43 +0300

    [1.20.1] 20.1.38: EntityBilly has its model set for particle emitter;

--- src/main/java/org/zeith/hammeranims/core/contents/entity/EntityBilly.java
+++ src/main/java/org/zeith/hammeranims/core/contents/entity/EntityBilly.java
@@ -14,6 +14,7 @@ import net.neoforged.neoforge.event.entity.EntityAttributeCreationEvent;
 import org.zeith.hammeranims.api.animsys.AnimationSystem;
 import org.zeith.hammeranims.api.animsys.CommonLayerNames;
 import org.zeith.hammeranims.api.animsys.layer.AnimationLayer;
+import org.zeith.hammeranims.api.geometry.IGeometryContainer;
 import org.zeith.hammeranims.api.tile.IAnimatedEntity;
 import org.zeith.hammeranims.core.init.ContainersHA;
 
@@ -74,6 +75,12 @@ public class EntityBilly
 				new AnimationLayer.Builder(CommonLayerNames.AMBIENT),
 				new AnimationLayer.Builder(CommonLayerNames.LEGS)
 		);
+	}
+	
+	@Override
+	public IGeometryContainer getObjectModel()
+	{
+		return ContainersHA.BILLY_GEOM;
 	}
 	
 	@Override
