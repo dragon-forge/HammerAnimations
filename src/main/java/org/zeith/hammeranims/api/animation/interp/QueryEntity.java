d01264659b2e4410bf713fdb3cc479bbc2dbdbb8
Zeith <me@zeith.org>
2026-05-08 12:25:23 +0300

    [1.21.1] 21.1.41: Added several more entity query variables.

--- src/main/java/org/zeith/hammeranims/api/animation/interp/QueryEntity.java
+++ src/main/java/org/zeith/hammeranims/api/animation/interp/QueryEntity.java
@@ -2,10 +2,12 @@ package org.zeith.hammeranims.api.animation.interp;
 
 import dev.zeith.lzvm.molang.compiler.libs.MoMathLibrary;
 import dev.zeith.lzvm.op.ReadonlyLzVarOp;
+import net.minecraft.core.Direction;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.navigation.*;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.phys.Vec3;
 import org.zeith.hammeranims.api.tile.IAnimatedEntity;
 
 import java.util.function.BiConsumer;
@@ -56,14 +58,35 @@ public class QueryEntity
 		reg.accept("query.is_invisible", ofBool(entity::isInvisible));
 		reg.accept("query.is_silent", ofBool(entity::isSilent));
 		reg.accept("query.is_sneaking", ofBool(entity::isCrouching));
+		reg.accept("query.is_in_contact_with_water", ofBool(entity::isInWaterOrRain));
 		reg.accept("query.is_swimming", ofBool(entity::isSwimming));
 		reg.accept("query.has_player_rider", ofBool(() -> entity.hasPassenger(Player.class::isInstance)));
 		reg.accept("query.has_owner", ofBool(() -> entity instanceof OwnableEntity own && own.getOwnerUUID() != null));
+		reg.accept("query.ground_speed", () -> entity.getDeltaMovement().horizontalDistance());
+		reg.accept("query.vertical_speed", () -> entity.getDeltaMovement().y());
+		reg.accept("query.yaw_speed", () -> entity.getYRot() - entity.yRotO);
+		reg.accept("query.movement_direction", () -> entity.getDeltaMovement().horizontalDistance() > 0.001 ? getNearest(entity.getDeltaMovement()).get3DDataValue() : 6);
+		reg.accept("query.rider_body_x_rotation", () ->
+				!entity.isVehicle() || entity.getFirstPassenger() == null
+				? 0
+				: entity.getFirstPassenger() instanceof LivingEntity
+				  ? 0
+				  : entity.getFirstPassenger().getViewXRot(partialTicks)
+		);
+		reg.accept("query.rider_body_y_rotation", () ->
+				!entity.isVehicle() || entity.getFirstPassenger() == null
+				? 0
+				: entity.getFirstPassenger() instanceof LivingEntity living
+				  ? MoMathLibrary.lerp(living.yBodyRotO, living.yBodyRot, partialTicks)
+				  : entity.getFirstPassenger().getViewYRot(partialTicks)
+		);
+		reg.accept("query.rider_head_x_rotation", () -> entity.getFirstPassenger() instanceof LivingEntity le ? le.getViewXRot(partialTicks) : 0);
+		reg.accept("query.rider_head_y_rotation", () -> entity.getFirstPassenger() instanceof LivingEntity le ? le.getViewXRot(partialTicks) : 0);
 		
 		if(entity instanceof LivingEntity)
 		{
 			LivingEntity le = (LivingEntity) entity;
-			reg.accept("query.body_x_rotation", () -> le.getViewXRot(partialTicks));
+			reg.accept("query.body_x_rotation", () -> 0);
 			reg.accept("query.body_y_rotation", () -> MoMathLibrary.lerp(le.yBodyRotO, le.yBodyRot, partialTicks));
 			reg.accept("query.blocking", ofBool(le::isBlocking));
 			reg.accept("query.health", le::getHealth);
@@ -78,14 +101,18 @@ public class QueryEntity
 			reg.accept("query.scale", le::getScale);
 			reg.accept("query.item_remaining_use_duration", () -> (le.getUseItemRemainingTicks() + partialTicks) / 20.0);
 			reg.accept("query.sleep_rotation", () -> le.getBedOrientation() != null ? (double) le.getBedOrientation().toYRot() : 0.0);
+			reg.accept("query.head_x_rotation", () -> le.getViewXRot(partialTicks));
+			reg.accept("query.head_y_rotation", () -> le.getViewYRot(partialTicks));
 		} else
 		{
+			reg.accept("query.body_x_rotation", () -> entity.getViewXRot(partialTicks));
 			reg.accept("query.body_y_rotation", () -> entity.getViewYRot(partialTicks));
 		}
 		
 		if(entity instanceof Mob)
 		{
 			Mob mob = (Mob) entity;
+			reg.accept("query.is_leashed", ofBool(mob::isLeashed));
 			reg.accept("query.has_target", ofBool(() -> mob.getTarget() != null));
 			reg.accept("query.can_climb", ofBool(() -> !mob.isNoAi() && mob.getNavigation() instanceof WallClimberNavigation));
 			reg.accept("query.can_fly", ofBool(() -> !mob.isNoAi() && mob.getNavigation() instanceof FlyingPathNavigation));
@@ -104,5 +131,10 @@ public class QueryEntity
 			NeutralMob n = (NeutralMob) entity;
 			reg.accept("query.is_angry", ofBool(n::isAngry));
 		}
+	}
+	
+	private static Direction getNearest(Vec3 v)
+	{
+		return Direction.getNearest(v.x, v.y, v.z);
 	}
 }
