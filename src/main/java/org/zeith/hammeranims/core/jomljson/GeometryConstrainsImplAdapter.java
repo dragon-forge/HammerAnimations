b98d123eb6289f0baa50e9d066c976853a8f2298
Zeith <me@zeith.org>
2026-06-10 09:00:08 +0300

    Fix compile errors.

--- src/main/java/org/zeith/hammeranims/core/jomljson/GeometryConstrainsImplAdapter.java
+++ src/main/java/org/zeith/hammeranims/core/jomljson/GeometryConstrainsImplAdapter.java
@@ -1,11 +1,9 @@
 package org.zeith.hammeranims.core.jomljson;
 
 import org.json.JSONObject;
-import org.zeith.hammeranims.core.impl.api.geometry.constrains.BoneConstraintsImpl;
-import org.zeith.hammeranims.core.impl.api.geometry.constrains.GeometryConstrainsImpl;
+import org.zeith.hammeranims.core.impl.api.geometry.constrains.*;
 import org.zeith.hammeranims.core.utils.GsonHelper;
 
-import java.util.HashMap;
 import java.util.*;
 
 public class GeometryConstrainsImplAdapter
@@ -16,19 +14,5 @@ public class GeometryConstrainsImplAdapter
 		for(var e : object.keySet())
 			builder.put(e, BoneConstrainsImplAdapter.parse(GsonHelper.getAsJsonObject(object, e)));
 		return new GeometryConstrainsImpl(Map.copyOf(builder));
-		JsonObject object = json.getAsJsonObject();
-		ImmutableMap.Builder<String, BoneConstraintsImpl> builder = ImmutableMap.builder();
-		for(Map.Entry<String, JsonElement> e : object.entrySet())
-			builder.put(e.getKey().toLowerCase(Locale.ROOT), context.deserialize(e.getValue(), BoneConstraintsImpl.class));
-		return new GeometryConstrainsImpl(builder.build());
-	}
-	
-	@Override
-	public JsonElement serialize(GeometryConstrainsImpl src, Type typeOfSrc, JsonSerializationContext context)
-	{
-		JsonObject object = new JsonObject();
-		for(Map.Entry<String, BoneConstraintsImpl> e : src.bones().entrySet())
-			object.add(e.getKey(), context.serialize(e.getValue(), BoneConstraintsImpl.class));
-		return object;
 	}
 }
