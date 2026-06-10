f3e0b0b4a0b14b18a6eff50593fb14e11bac1b31
Zeith <me@zeith.org>
2026-06-10 09:20:50 +0300

    1.0.56

--- src/main/java/org/zeith/hammeranims/HammerAnimations.java
+++ src/main/java/org/zeith/hammeranims/HammerAnimations.java
@@ -2,6 +2,7 @@ package org.zeith.hammeranims;
 
 import org.zeith.hammeranims.api.animation.IAnimationContainer;
 import org.zeith.hammeranims.api.geometry.IGeometryContainer;
+import org.zeith.hammeranims.core.init.DefaultsHA;
 import org.zeith.hammeranims.core.utils.InstanceHelpers;
 import org.zeith.hammeranims.standalone.wasm.Log;
 
@@ -10,7 +11,7 @@ import java.util.Optional;
 
 public class HammerAnimations
 {
-	public static final String VERSION = "1.0.47";
+	public static final String VERSION = "1.0.56";
 	
 	public static final Log LOG = new Log();
 	
@@ -21,6 +22,28 @@ public class HammerAnimations
 		if(initialized) return;
 		long start = System.currentTimeMillis();
 		initialized = true;
+		
+		HammerAnimations.loadGeo("null", """
+			{
+				"format_version": "1.12.0",
+				"minecraft:geometry": [
+					{
+						"description": {
+							"identifier": "geometry.null",
+							"texture_width": 1,
+							"texture_height": 1,
+							"visible_bounds_width": 0,
+							"visible_bounds_height": 0,
+							"visible_bounds_offset": [0, 0, 0]
+						},
+						"bones": []
+					}
+				]
+			}"""
+		);
+		
+		LOG.info("Register {}", DefaultsHA.NULL_GEOMETRY.getRegistryKey());
+		
 		LOG.info("HammerAnimations {} initialized in {} ms.", VERSION, System.currentTimeMillis() - start);
 	}
 	
